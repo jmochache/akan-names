74112d28957eef9d1a6d5b256055a68f569502b1
Mochache Nyangweso <JMOCHACHE@safaricom.co.ke>
2022-10-08 18:21:59 +0300

    add constants

--- js/index.js
+++ js/index.js
@@ -1,1 +1,5 @@
-const = {}
+const maleNames = ("Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame")
+const femaleNames = ("Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama")
+const weekNames = ("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
+
+daysOfTheWeek = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7
