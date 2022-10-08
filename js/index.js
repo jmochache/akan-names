ad794969d64487cba293bec49d76b98d5f8a0af4
Mochache Nyangweso <JMOCHACHE@safaricom.co.ke>
2022-10-08 18:53:49 +0300

    add variables to javascript

--- js/index.js
+++ js/index.js
@@ -1,5 +1,14 @@
-const maleNames = ("Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame")
-const femaleNames = ("Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama")
-const weekNames = ("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
+const maleNames = ("Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame");
+const femaleNames = ("Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama");
+const weekNames = ("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
 
 daysOfTheWeek = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7
+
+var yearOfBirth = document.getElementById("yearOfBirth").value;
+console.log(yearOfBirth);
+
+var monthOfBirth = document.getElementById("monthOfBirth").value;
+console.log(monthOfBirth);
+
+var dayOfBirth = document.getElementById("dayOfBirth").value;
+console.log(dayOfBirth);
