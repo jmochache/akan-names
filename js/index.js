01a0347166de152d260563af345cb1e6d1acc035
Mochache Nyangweso <JMOCHACHE@safaricom.co.ke>
2022-10-11 06:22:35 +0300

    modify files

--- js/index.js
+++ js/index.js
@@ -1,57 +1,95 @@
-const maleNames = [
-  "Kwasi",
-  "Kwadwo",
-  "Kwabena",
-  "Kwaku",
-  "Yaw",
-  "Kofi",
-  "Kwame",
-];
-const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-const weekNames = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
+// Mochache akan code
 
-function getNameAndDay(yearOfBirth, dayOfBirth, monthOfBirth, century, gender) {
-  var indexOfTheDay =
-    Math.round(
-      century / 4 -
-        2 * century -
-        1 +
-        (5 * yearOfBirth) / 4 +
-        (26 * (monthOfBirth + 1)) / 10 +
-        day
-    ) % 7;
-  var bornDayUser = weekNames[indexOfTheDay];
-  var name = null;
+function getAkanName () {
+  var birthYear = document.getElementById("year-input").value;
+  var birthMonth = Number(document.getElementById("month-input").value);
+  var birthDay = Number(document.getElementById("day-input").value);
+  var genders = document.getElementsByName("gender");
 
-  if (gender === "M") {
-    name = maleNames[indexOfTheDay];
-  } else if (gender === "F") {
-    name = femaleNames[indexOfTheDay];
+  // function to get gender
+  function getGender () {
+    for (var gender of genders){
+      if (gender.checked){
+        return gender.value;
+      }
+    }
+  }
+
+  var selectedGender = getGender();
+  console.log(selectedGender);
+
+  // validation functions
+  function monthIsValidator () {
+    if (birthMonth < 1 || birthMonth > 12) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  function dayIsValidator () {
+    if (birthMonth === 2 && Number(birthYear)%4 === 0) {
+      if (birthDay > 28 || birthDay < 1) {
+        return false;
+      } else if (birthMonth === 2 && birthDay > 29) {
+        return false;
+      } else if (birthMonth === 2 && birthDay < 1) {
+        return false;
+      } else {
+        return true;
+      }
+    } else if (birthDay < 1 || birthDay > 31){
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  //validation variables
+  var monthIsValid = monthIsValidator();
+  var dayIsValid = dayIsValidator();
+
+  //formula to get day of birth indes (Sunday = 1, Monday = 2)etc..
+  var weekDaysIndex = Math.floor((((Number(birthYear.slice(0,2))/4)-2*Number(birthYear.slice(0,2))-1)+
+          ((5*Number(birthYear.slice(2,4))/4))+((26*(birthMonth+1)/10))+birthDay)%7);
+
+  //constants as provided
+  var weekDays = [
+    "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+  ];
+
+  var maleNames = [
+    "Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"
+  ];
+
+  var femaleNames = [
+    "Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"
+  ];
+
+  //generating index value to select items on arrays
+  var index;
+  // fix formula bug
+  if (weekDaysIndex == 0){
+    index = 6;
   } else {
-    alert("Invalid input please try again");
+    index = weekDaysIndex - 1;
   }
 
-  if (month < 1 || month > 12 || month == "") {
-    alert("Invalid input, please try again");
-  }
+  console.log(index);
 
-  if (day < 1 || day > 31 || day == "") {
-    alert("Invalid input, please try again");
+  if (selectedGender == "male" && monthIsValid && dayIsValid) {
+    document.getElementById('result').textContent = "You were born on a " + weekDays[index] + " , your Akan name is " + maleNames[index];
+    document.getElementById('display-name').textContent = "Here is your Akan name: ";
+    document.getElementById('result').style.fontSize = "18px";
+    document.querySelector('h1').textContent = "Hello" + " " + maleNames[index];
+    return false;
+  } else if (selectedGender == "female" && monthIsValid && dayIsValid) {
+    document.getElementById('result').textContent = "You were born on a " + weekDays[index] + " , your Akan name is " + femaleNames[index];
+    document.getElementById('display-name').textContent = "Here is your Akan name: ";
+    document.getElementById('result').style.fontSize = "18px";
+    document.querySelector('h1').textContent = "Hello" + " " + femaleNames[index];
+    return false;
+  } else {
+    alert("Invalid entry, try again");
   }
-
-  if (year < 1900 || year > 2022 || year == "") {
-    alert("Invalid output, please try again");
-  }
-
-  var list = [bornDayUser, name];
-  return list;
 }
-
